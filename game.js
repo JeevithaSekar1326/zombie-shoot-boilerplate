c4d8244dc726fdb0610722e5c4c9d39435a132cf
JeevithaSekar1326 <159133001+JeevithaSekar1326@users.noreply.github.com>
2024-05-14 09:19:36 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -17,3 +17,100 @@
 // Iteration 6: Write a code to start the game by calling the first zombie
 
 // Iteration 7: Write the helper function to get random integer
+
+let score = 0;
+let lives = 3;
+let timeLeft = 60; 
+
+const shotgunSound = new Audio('bgm.mp3');
+
+
+const backgroundSound = new Audio('shotgun.mp3');
+backgroundSound.loop = true;
+backgroundSound.play();
+
+
+const MAX_ZOMBIE_ESCAPES = 4;
+
+
+function createZombie() {
+    const zombie = document.createElement('div');
+    zombie.classList.add('zombie');
+   
+    const maxX = window.innerWidth - 100; 
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * (window.innerHeight - 100)); 
+    zombie.style.left = `${randomX}px`;
+    zombie.style.top = `${randomY}px`;
+    document.body.appendChild(zombie);
+
+   
+    setTimeout(() => {
+        if (!zombie.classList.contains('shot')) {
+            zombie.remove();
+            if (lives > 0) {
+                lives--; 
+                if (lives === 0) {
+                   
+                    gameOver();
+                }
+            }
+        }
+    }, 5000); 
+}
+
+
+function missedZombie() {
+    if (lives > 0) {
+        lives--;
+        if (lives === 0) {
+          
+            gameOver();
+        }
+    }
+}
+
+function destroyZombie(zombie) {
+    if (!zombie.classList.contains('shot')) {
+        zombie.classList.add('shot');
+        score++; 
+        shotgunSound.play(); 
+        zombie.remove();
+    }
+}
+
+
+let timerInterval;
+
+function startTimer() {
+    timerInterval = setInterval(() => {
+        timeLeft--;
+        if (timeLeft === 0) {
+            clearInterval(timerInterval);
+          
+            gameOver();
+        }
+    }, 1000);
+}
+
+
+function startGame() {
+    startTimer();
+    createZombie();
+}
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function gameOver() {
+   
+    clearInterval(timerInterval);
+    
+}
+
+
+startGame();
+
